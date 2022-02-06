c34273b290b056a543f263590a1f97a80ea0e197
yongjunjeong99 <young990409@naver.com>
2022-02-06 18:48:35 +0900

    Create Project

---
+++ main.js
@@ -0,0 +1,15 @@
+const str = `
+010-1234-5678
+yong@naver.com
+the
+"https:www.sadadadsasdasd"
+The quick brown fox dog jumps.
+abbcccdddd
+d
+hxyp
+http://localhost
+`
+
+console.log(
+  str.match(/d{2}/)
+)
